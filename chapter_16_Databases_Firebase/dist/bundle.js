7ef424e2b4ca83ad521a66fe8ce0a6813483163c
Austin Matthews <austm14@gmail.com>
2024-02-26 12:10:56 -0500

    Adding firebase into chapter 16

---
+++ chapter_16_Databases_Firebase/dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("const firebaseConfig = {\r\n  apiKey: \"AIzaSyAatekgVrBm9D_EbYUFlkv9nn9OOmZ3ACE\",\r\n  authDomain: \"udemy-modern-javascript-273bb.firebaseapp.com\",\r\n  projectId: \"udemy-modern-javascript-273bb\",\r\n  storageBucket: \"udemy-modern-javascript-273bb.appspot.com\",\r\n  messagingSenderId: \"764386409334\",\r\n  appId: \"1:764386409334:web:4206a7925b84e3680dd857\",\r\n  measurementId: \"G-YP84GLZRF4\"\r\n};\r\n\r\n\n\n//# sourceURL=webpack://chapter_16_databases_firebase/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ chapter_16_Databases_Firebase/src/index.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAatekgVrBm9D_EbYUFlkv9nn9OOmZ3ACE",
+  authDomain: "udemy-modern-javascript-273bb.firebaseapp.com",
+  projectId: "udemy-modern-javascript-273bb",
+  storageBucket: "udemy-modern-javascript-273bb.appspot.com",
+  messagingSenderId: "764386409334",
+  appId: "1:764386409334:web:4206a7925b84e3680dd857",
+  measurementId: "G-YP84GLZRF4"
+};
+
